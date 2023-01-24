1faf10ec9bb341b5f0f24ecbe4622d2fcdf945ca
Christ Ghosh <christghosh79@gmail.com>
2023-01-24 21:14:41 +0200

    ChristGhosh

---
+++ Problems.java
@@ -0,0 +1,7 @@
+public class Problems{
+	public statis void main(String[] args){
+		System.out.println("!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello, there are a lot);
+		System.out.print("of problems);
+	}
+}
