166e70c48f0814c251de2178c6b11a86f5930725
Christ Ghosh <123003394+ChristGhosh@users.noreply.github.com>
2023-04-12 11:19:50 +0300

    Update Me.java

--- Me.java
+++ Me.java
@@ -2,7 +2,7 @@ public class Me{
 	public static void main(String [] args){
 		System.out.println("My name: Christ Ghosh");
 		System.out.println("My age: 18");
-		System.out.println("My age: Computer Engineering");
+		System.out.println("My major is: Computer Engineering");
 		System.out.println("Year of study: 2023"); 
 	}
 }
